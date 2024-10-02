105f1ca3370d3ecf363d4e4e1f7a15461b3a209f
Syahid <earlsyahid@gmail.com>
2024-10-02 15:08:00 +0700

    Update UTS4.cpp

--- UTS4.cpp
+++ UTS4.cpp
@@ -1,26 +1,33 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
+
 int main()
 {
-    int m,i;
-    cout<<"Masukkan angka: ";
-    cin>>m;
 
-    int factors[1000], count;
+  int batas, i;
 
-    cout << "\nBilangan Ganjil: " <<endl;
-    for (int i = 1; i < m; i++) {
-        for (int j = 1; j < count; j++) {
-        }
-        cout << "\n";
+  cout << "Masukkan Angka : ";
+  cin >> batas;
+
+  cout << "Bilangan Genap :" << endl;
+  for (i = 1; i <= batas; i++)
+  {
+      if (i % 2 == 0)
+    {
+    cout << i << " " << endl;
     }
-
-    cout << "\nBilangan Genap: " <<endl;
-    for (int i = 1; i < m; i++) {
-        for (int j = 1; j < count; j++) {
-            cout << factors[j] << " ";
-        }
-        cout << "\n";
+  }
+  cout << endl;
+  cout << "Bilangan Ganjil :" << endl;
+  for (i = 1; i <= batas; i++)
+  {
+      if (i % 2 == 1)
+    {
+        cout << i << " " << endl;
     }
+  }
 
+  cout << endl;
+  return 0;
 }
